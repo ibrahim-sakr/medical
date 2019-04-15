439e10e5faf20e6218ce4fb883568d72bbb05bbf
hadeer asker <hader.asker@hmasoft.com>
2019-04-15 10:54:21 +0200

    notification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,9 @@ use App\Mail\Newsletter;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Messaging\CloudMessage;
 
 class UserController extends Controller
 {
@@ -34,11 +37,30 @@ class UserController extends Controller
     public function sendMail(Request $request)
     {
         $subject = 'reservation';
-        $message ='hello' ;
+        $message = 'hello';
 
         $mail = new Newsletter($subject, $message);
         Mail::to('eng.hader2012@gmail.com')->send($mail);
 
         $user = $this->store($request->all());
+    }
+
+    public function sendNotification(Request $request)
+    {
+        $serviceAccount = ServiceAccount::fromJsonFile(base_path() . '/medical-system-firebase.json');
+        $firebase = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            ->create();
+
+        $topic = 'a-topic';
+        $notification = $request->notification;
+        $data =$request->data;
+        $message = CloudMessage::fromArray([
+            'topic' => $topic,
+//            'notification' => [$notification], // optional
+//            'data' => [$data], // optional
+        ]);
+        $messaging = $firebase->getMessaging();
+        $messaging->send($message);
     }
 }
